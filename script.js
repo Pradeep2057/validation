8078aeeb9128e6959a55cbafbf117794e46767f0
Pradeep2057 <51450053+Pradeep2057@users.noreply.github.com>
2020-06-24 17:23:19 +0545

    Error Corrected

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const userInput = document.getElementById("password").value;
-
-const numCheck = /^(?=.*[0-9])$/;
-const caseCheck = /^(?=.*[A-Z])$/;
-const charCheck = /^(?=.*[!@#$%&^*])$/;
-
 function validation(){
-if(userInput.length >= 8){
+const userInput = document.querySelector("#password").value;
+
+if(userInput.length >=8){
     document.getElementById("length").style.color="green";
 }else{
     document.getElementById("length").style.color="red";
 }
 
+let numCheck =  /[0-9]/g;
 if(numCheck.test(userInput)){
     document.getElementById("number").style.color="green";
 }else{
     document.getElementById("number").style.color="red"; 
 }
 
+let caseCheck = /[A-Z]/g;
 if(caseCheck.test(userInput)){
-    document.getElementById("case").style.color="greem";
+    document.getElementById("case").style.color="green";
 }else{
     document.getElementById("case").style.color="red"; 
 }
+
+let charCheck =/[!@#$%^&*]/g ;
 if(charCheck.test(userInput)){
     document.getElementById("char").style.color="green";
 }else{
